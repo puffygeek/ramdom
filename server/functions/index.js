f4d7f48d8193456771e51ccfc2f8c90c1f5dc3a2
Sagiv Ofek <sagiv4@gmail.com>
2019-01-06 20:42:52 -0500

    a

--- server/functions/index.js
+++ server/functions/index.js
@@ -27,12 +27,27 @@ exports.webhook = functions.https.onRequest((req, res) => {
    const data = {
     from: 'RANDOM <hello@randomny.com>',
     to: email,
-    subject: 'Random event details',
-    text: `Now we can tell you the full details:
-Midtown Manhattan 2/7/19 at 7pm
-See you soon
-    `,
-  };
+    subject: 'Congratulations! You are ready to join the Random',
+    text: `
+Welcome to the Random! Here are the event details:
+
+Time: Thursday 2/7/19 at 8pm
+Location: 42 W 38th Street, New York, NY, 10018 | Suite #1001,
+
+Here are some details which will give you some idea about what is going to happen:
+
+- When you enter the building, tell the doorman that you are here for suite #1001, he/she will direct you to the 10th floor.
+- Upon entering the space, you will be greeted by Random faithful assistants. Do not speak with them, unless you have a specific question, to which they will try to answer without talking to you. Their only job is to observe and assist with the basic logistics of running of the experience, so please do not make any attempt to create an interaction with them.
+- They will give you name tags upon which you can write either you personal name or the name you wished to be called that night. You will also get a number. Random will not use your name during the experience, but call you by your number.
+- The assistants will also give you masks to wear, and ask you to let go of your phone for the two hours of the experience. This is essential to make sure everybody has a good time. If you are expecting any sort of urgent communication or emergency, it might be a good idea not to attend Random that night.
+- Please do not enter the experience space until the assistants allow you to, and wait patiently for them to take care of other guests before getting to you.
+- When entering the space, you can start interacting with anyone and everyone. Please maintain all common sense rules of conduct and be respectful of other people. 
+- During the two hours of the experience, you will be called to perform collaborative tasks together. You will receive those instructions via the big screen, please pay attention to it during the entire night.
+- Participants will be called one by one into the computer room. In the computer room each participant will receive his/her secret objective from Random and it will be up to them to make the most of it.
+
+See you soon,
+Random
+`,};
 
   mailgun.messages().send(data, (error, body) => {
     console.log('email sent', body)
